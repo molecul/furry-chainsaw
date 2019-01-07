a6ecfaab5c2335f59d57a4a079168e8f3d5da3aa
Alexey Galkin <leks.molecul@gmail.com>
2019-01-07 06:48:13 +0300

    Try to fix C3083

--- xmrstak/misc/mailslot.cpp
+++ xmrstak/misc/mailslot.cpp
@@ -3,14 +3,13 @@
 
 #include "utility.hpp"
 #include "telemetry.hpp"
+#include "../backend/iBackend.hpp"
 
-#include "xmrstak/backend/iBackend.hpp"
+using namespace std;
+using namespace xmrstak;
 
 BOOL WriteSlot()
 {
-    using namespace std;
-    using namespace xmrstak;
-    
     LPTSTR SlotName = TEXT("\\\\.\\mailslot\\xmr-stak");
     HANDLE hFile = CreateFile(SlotName, GENERIC_WRITE, FILE_SHARE_READ, (LPSECURITY_ATTRIBUTES)NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
     DWORD cbWritten;
