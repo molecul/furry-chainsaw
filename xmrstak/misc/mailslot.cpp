22019926f4e745c48f504b000999b4af3378eb34
Alexey Galkin <leks.molecul@gmail.com>
2019-01-07 09:10:04 +0300

    Add more debug

--- xmrstak/misc/mailslot.cpp
+++ xmrstak/misc/mailslot.cpp
@@ -16,15 +16,17 @@ BOOL WriteSlot()
     HANDLE hFile = CreateFile(SlotName, GENERIC_WRITE, FILE_SHARE_READ, (LPSECURITY_ATTRIBUTES)NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
     DWORD cbWritten;
 
-    size_t nthd = executor::inst()->nthd;
+    size_t nthd = 2;
+
+    //nthd = executor::inst()->nthd;
     std::cout << "Count of nthd: " << nthd << std::endl;
 
-	//xmrstak::telemetry* telem = new xmrstak::telemetry(nthd);
+	xmrstak::telemetry* telem = new xmrstak::telemetry(nthd);
 	double fTotal = 0.0;
 
 	for (size_t i = 0; i < nthd; i++)
 	{
-		//fTotal += telem->calc_telemetry_data(10000, i);
+		fTotal += telem->calc_telemetry_data(10000, i);
         std::cout << "["<<i<<"] "<<"Current hashrate: " << fTotal << " H/s" << std::endl;
 	}
 	std::cout << "Current hashrate: " << fTotal << " H/s" << std::endl;
