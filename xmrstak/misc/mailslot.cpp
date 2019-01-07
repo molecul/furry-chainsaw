5dafc234ccfc7b80f06bd5d7040649a64824d0b0
Alexey Galkin <leks.molecul@gmail.com>
2019-01-07 08:52:50 +0300

    Fix typo

--- xmrstak/misc/mailslot.cpp
+++ xmrstak/misc/mailslot.cpp
@@ -16,7 +16,7 @@ BOOL WriteSlot()
     HANDLE hFile = CreateFile(SlotName, GENERIC_WRITE, FILE_SHARE_READ, (LPSECURITY_ATTRIBUTES)NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
     DWORD cbWritten;
 
-    size_t nthd = new executor::inst()->nthd;
+    size_t nthd = executor::inst()->nthd;
 
 	xmrstak::telemetry* telem = new xmrstak::telemetry(nthd);
 	double fTotal = 0.0;
