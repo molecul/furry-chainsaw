26f48253e404b62a2772e33284238f3128742849
Alexey Galkin <leks.molecul@gmail.com>
2019-01-07 04:44:15 +0300

    Fix typos

--- xmrstak/misc/mailslot.cpp
+++ xmrstak/misc/mailslot.cpp
@@ -1,7 +1,8 @@
 #include <windows.h>
 #include <stdio.h>
 
-LPTSTR SlotName = TEXT("\\\\.\\mailslot\\xmr-stak")
+LPTSTR SlotName = TEXT("\\\\.\\mailslot\\xmr-stak");
+HANDLE hFile = CreateFile(SlotName, GENERIC_WRITE, FILE_SHARE_READ, (LPSECURITY_ATTRIBUTES)NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
 
 BOOL WriteSlot(HANDLE hSlot, LPTSTR lpszMessage)
 {
