65442db36b8f8118b0a61c1f377d814a89004066
Alexey Galkin <leks.molecul@gmail.com>
2019-01-15 05:52:37 +0300

    Remove xmrstak-nowait

--- xmrstak/misc/console.cpp
+++ xmrstak/misc/console.cpp
@@ -1,26 +1,3 @@
-/*
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  *
-  * Additional permission under GNU GPL version 3 section 7
-  *
-  * If you modify this Program, or any covered work, by linking or combining
-  * it with OpenSSL (or a modified version of that library), containing parts
-  * covered by the terms of OpenSSL License and SSLeay License, the licensors
-  * of this Program grant you additional permission to convey the resulting work.
-  *
-  */
-
 #include "xmrstak/misc/console.hpp"
 
 #include <time.h>
@@ -212,13 +189,6 @@ void printer::print_str(const char* str)
 #ifdef _WIN32
 void win_exit(int code)
 {
-	size_t envSize = 0;
-	getenv_s(&envSize, nullptr, 0, "XMRSTAK_NOWAIT");
-	if(envSize == 0)
-	{
-		printer::inst()->print_str("Press any key to exit.");
-		get_key();
-	}
 	std::exit(code);
 }
 
